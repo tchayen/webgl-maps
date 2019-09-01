cbd0a1f2506db1579a918e60f2cb7d0c7d557ee1
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2019-09-02 01:20:22 +0200

    Finish rendering font/ in browser

---
+++ font/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
